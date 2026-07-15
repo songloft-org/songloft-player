9cac5d85fbfd837d70cf14486dc0c230edcc75aa
涵曦 <im.hanxi@gmail.com>
2026-07-15 10:33:59 +0800

    test: 移除未使用的 flutter_localizations import

--- test/features/library/song_list_tile_desktop_test.dart
+++ test/features/library/song_list_tile_desktop_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:songloft_flutter/features/library/presentation/providers/favorite_provider.dart';
